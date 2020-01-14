5ece3cb6cfd66a7cf889f145b151bcf0c1f5a6b9
Trey Suiter <trey.suiter@gmail.com>
2020-01-13 19:59:07 -0600

    cash to coins complete

--- cashToCoins.py
+++ cashToCoins.py
@@ -1,10 +1,13 @@
-
-# Your magic Python code here
-
-import math
+# Turn you cash into coins
 
 def cashToCoins():
+
     dollarAmount = 8.69
+
+    #Convert dollars to whole number cuz python ain't got time for decimals
+    dollarAmount *= 100
+    
+    #Initialize dictionary
     piggyBank = {
         "pennies": 0,
         "nickels": 0,
@@ -12,22 +15,19 @@ def cashToCoins():
         "quarters": 0
     }
 
-    piggyBank["quarters"] = int(dollarAmount // .25)
-    print(dollarAmount)
-    dollarAmount -= piggyBank["quarters"] * .25
-    print(piggyBank["quarters"] * .25)
-    print(dollarAmount)
-
-    piggyBank["dimes"] = int(dollarAmount // .10)
-    dollarAmount -= piggyBank["dimes"] * .10
-
-    piggyBank["nickels"] = int(dollarAmount // .05)
-    dollarAmount -= piggyBank["nickels"] * .05
-
-    piggyBank["pennies"] = int(dollarAmount / .01)
+    #Use integer division to get coin totals and subtract value as you 
+    #decrease in denomination 
+    piggyBank["quarters"] = int(dollarAmount // 25)
+    dollarAmount -= piggyBank["quarters"] * 25
     
+    piggyBank["dimes"] = int(dollarAmount // 10)
+    dollarAmount -= piggyBank["dimes"] * 10
 
+    piggyBank["nickels"] = int(dollarAmount // 5)
+    dollarAmount -= piggyBank["nickels"] * 5
+
+    piggyBank["pennies"] = int(dollarAmount / 1)
+    
     print(piggyBank)
-
 
 cashToCoins()
